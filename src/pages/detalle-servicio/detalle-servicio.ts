7ec2f7e3f6a9e3be478e59714cf570df219af752
Wendell Arias <wdarias@unicesar.edu.co>
2018-08-31 14:16:55 -0500

    consultar servicio

--- src/pages/detalle-servicio/detalle-servicio.ts
+++ src/pages/detalle-servicio/detalle-servicio.ts
@@ -1,12 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the DetalleServicioPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { ServicioProvider  } from '../../providers/servicio/servicio';
+import { Servicio } from '../../models/servicio.model';
 
 @Component({
   selector: 'page-detalle-servicio',
@@ -15,14 +11,33 @@ import { NavController, NavParams } from 'ionic-angular';
 export class DetalleServicioPage {
 
   IdServicio : number;
+  servicio : Servicio;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+      private servicioProvider: ServicioProvider) {
     // recibir valor del servicio
-     this.IdServicio = this.navParams.get('IdServicio');
+     this.IdServicio = this.navParams.get('IdServicio');  
+     this.servicio = new Servicio();   
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad DetalleServicioPage');
+    console.log('ionViewDidLoad DetalleServicioPage');    
+    this.consultarServicio();
+  }
+
+  consultarServicio(){
+    this.servicioProvider.get(this.IdServicio).subscribe(
+      result => {   
+        if (result == null){        
+          return;
+        }    
+        this.servicio = result;
+        console.log(this.servicio);
+      },
+      error => {
+        console.log(<any>error);
+      }
+    );
   }
 
 }
